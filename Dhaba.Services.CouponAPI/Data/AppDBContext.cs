64c49a129a47ddbc4418092018f2ccf5a2dcac30
bhupeshlahare <bhupeshlahare@gmail.com>
2024-04-12 21:39:52 +0530

    Coupon DBContext

---
+++ Dhaba.Services.CouponAPI/Data/AppDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Dhaba.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dhaba.Services.CouponAPI.Data
+{
+	public class AppDBContext:DbContext
+	{
+		public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) 
+		{
+		}
+
+		public DbSet<Coupon> Coupons { get; set; }
+	}
+}
