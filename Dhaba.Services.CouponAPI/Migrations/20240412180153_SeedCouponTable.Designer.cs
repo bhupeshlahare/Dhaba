781847a74410175f34cf4f2059899e0a512459e2
bhupeshlahare <bhupeshlahare@gmail.com>
2024-04-12 23:41:30 +0530

    SeedCouponTable Migration

--- Dhaba.Services.CouponAPI/Migrations/20240412180153_SeedCouponTable.Designer.cs
+++ Dhaba.Services.CouponAPI/Migrations/20240412180153_SeedCouponTable.Designer.cs
@@ -3,6 +3,7 @@ using Dhaba.Services.CouponAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Dhaba.Services.CouponAPI.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240412180153_SeedCouponTable")]
+    partial class SeedCouponTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
